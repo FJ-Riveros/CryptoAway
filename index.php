fb1be56d51da6e13a85438c0a264b2a48edeb8bd
FJ-Riveros <fran.etnies@gmail.com>
2022-05-11 23:15:38 +0200

    Changing the style of the landing page

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Vacation - Free Bootstrap 4 Template by Colorlib</title>
+    <title>CryptoAway</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
@@ -23,7 +23,7 @@
   <body>
 	  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 	    <div class="container">
-	      <a class="navbar-brand" href="index.html">Vacation<span>Travel Agency</span></a>
+	      <a class="navbar-brand" href="index.html">CryptoAway<span>Travel with Crypto!</span></a>
 	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
 	        <span class="oi oi-menu"></span> Menu
 	      </button>
@@ -31,9 +31,8 @@
 	      <div class="collapse navbar-collapse" id="ftco-nav">
 	        <ul class="navbar-nav ml-auto">
 	          <li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
-	          <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
+	          <li class="nav-item"><a href="#about" class="nav-link">About</a></li>
 	          <li class="nav-item"><a href="destination.html" class="nav-link">Destination</a></li>
-	          <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
 	          <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
 	          <li class="nav-item cta"><a href="Modules/Login.php" class="nav-link">Login</a></li>
 	        </ul>
@@ -55,92 +54,10 @@
       </div>
     </div>
 
-    <section class="ftco-section ftco-no-pb ftco-no-pt">
-    	<div class="container">
-	    	<div class="row">
-					<div class="col-md-12">
-						<div class="search-wrap-1 ftco-animate p-4">
-							<form action="#" class="search-property-1">
-		        		<div class="row">
-		        			<div class="col-lg align-items-end">
-		        				<div class="form-group">
-		        					<label for="#">Destination</label>
-		          				<div class="form-field">
-		          					<div class="icon"><span class="ion-ios-search"></span></div>
-				                <input type="text" class="form-control" placeholder="Search place">
-				              </div>
-			              </div>
-		        			</div>
-		        			<div class="col-lg align-items-end">
-		        				<div class="form-group">
-		        					<label for="#">Check-in date</label>
-		        					<div class="form-field">
-		          					<div class="icon"><span class="ion-ios-calendar"></span></div>
-				                <input type="text" class="form-control checkin_date" placeholder="Check In Date">
-				              </div>
-			              </div>
-		        			</div>
-		        			<div class="col-lg align-items-end">
-		        				<div class="form-group">
-		        					<label for="#">Check-out date</label>
-		        					<div class="form-field">
-		          					<div class="icon"><span class="ion-ios-calendar"></span></div>
-				                <input type="text" class="form-control checkout_date" placeholder="Check Out Date">
-				              </div>
-			              </div>
-		        			</div>
-		        			<div class="col-lg align-items-end">
-		        				<div class="form-group">
-		        					<label for="#">Price Limit</label>
-		        					<div class="form-field">
-		          					<div class="select-wrap">
-		                      <div class="icon"><span class="ion-ios-arrow-down"></span></div>
-		                      <select name="" id="" class="form-control">
-		                        <option value="">$5,000</option>
-		                        <option value="">$10,000</option>
-		                        <option value="">$50,000</option>
-		                        <option value="">$100,000</option>
-		                        <option value="">$200,000</option>
-		                        <option value="">$300,000</option>
-		                        <option value="">$400,000</option>
-		                        <option value="">$500,000</option>
-		                        <option value="">$600,000</option>
-		                        <option value="">$700,000</option>
-		                        <option value="">$800,000</option>
-		                        <option value="">$900,000</option>
-		                        <option value="">$1,000,000</option>
-		                        <option value="">$2,000,000</option>
-		                      </select>
-		                    </div>
-				              </div>
-			              </div>
-		        			</div>
-		        			<div class="col-lg align-self-end">
-		        				<div class="form-group">
-		        					<div class="form-field">
-				                <input type="submit" value="Search" class="form-control btn btn-primary">
-				              </div>
-			              </div>
-		        			</div>
-		        		</div>
-		        	</form>
-		        </div>
-					</div>
-	    	</div>
-	    </div>
-    </section>
-
-    <section class="ftco-section services-section bg-light">
+    <section class="ftco-section services-section bg-light d-flex align-items-center min-vh-100" id="about">
       <div class="container">
-        <div class="row d-flex">
-          <div class="col-md-6 order-md-last heading-section pl-md-5 ftco-animate">
-          	<h2 class="mb-4">It's time to start your adventure</h2>
-            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
-            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.
-            A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-            <p><a href="#" class="btn btn-primary py-3 px-4">Search Destination</a></p>
-          </div>
-          <div class="col-md-6">
+        <div class="row d-flex justify-content-center">
+          <div class="col-md-8">
           	<div class="row">
           		<div class="col-md-6 d-flex align-self-stretch ftco-animate">
 		            <div class="media block-6 services d-block">
@@ -178,6 +95,10 @@
 		              </div>
 		            </div>      
 		          </div>
+				  <div class="col-md-12 heading-section ftco-animate">
+          			<h2 class="mb-4 text-center">It's time to start your adventure</h2>
+          		  	<p class="text-center" ><a href="#" class="btn btn-primary py-3 px-4">Search Destination</a></p>
+          		  </div>
           	</div>
           </div>
         </div>
@@ -188,7 +109,7 @@
     	<div class="container">
     		<div class="row d-flex">
     			<div class="col-md-6 d-flex">
-    				<div class="img d-flex align-self-stretch" style="background-image:url(img/about.jpg);"></div>
+					<div class="hero-wrap js-fullheight" style="background-image: url(img/about.jpg);" data-stellar-background-ratio="0.5">
     			</div>
     			<div class="col-md-6 pl-md-5 py-5">
     				<div class="row justify-content-start pb-3">
@@ -242,7 +163,7 @@
         			<a href="#" class="img" style="background-image: url(img/place-1.jpg);">
         				<div class="text">
         					<h3>Singapore</h3>
-        					<span>8 Tours</span>
+        					<!-- <span>8 Tours</span> -->
         				</div>
         			</a>
         		</div>
@@ -252,7 +173,7 @@
         			<a href="#" class="img" style="background-image: url(img/place-2.jpg);">
         				<div class="text">
         					<h3>Canada</h3>
-        					<span>2 Tours</span>
+        					<!-- <span>2 Tours</span> -->
         				</div>
         			</a>
         		</div>
@@ -262,7 +183,7 @@
         			<a href="#" class="img" style="background-image: url(img/place-3.jpg);">
         				<div class="text">
         					<h3>Thailand</h3>
-        					<span>5 Tours</span>
+        					<!-- <span>5 Tours</span> -->
         				</div>
         			</a>
         		</div>
@@ -272,7 +193,7 @@
         			<a href="#" class="img" style="background-image: url(img/place-4.jpg);">
         				<div class="text">
         					<h3>Autralia</h3>
-        					<span>5 Tours</span>
+        					<!-- <span>5 Tours</span> -->
         				</div>
         			</a>
         		</div>
@@ -281,11 +202,12 @@
     	</div>
     </section>
 
-    <section class="ftco-section testimony-section bg-bottom" style="background-image: url(img/bg_3.jpg);">
+    <!-- <section class="ftco-section testimony-section bg-bottom" style="background-image: url(img/journey.jpg);"> -->
+    <section class="ftco-section testimony-section bg-bottom hero-wrap " style="background-image: url(img/journey.jpg);"  data-stellar-background-ratio="0.5">
       <div class="container">
         <div class="row justify-content-center pb-4">
           <div class="col-md-7 text-center heading-section ftco-animate">
-            <h2 class="mb-4">Users Feedback</h2>
+            <h2 class="mb-4 text-white">Users Feedback</h2>
           </div>
         </div>
         <div class="row ftco-animate">
@@ -367,9 +289,10 @@
       </div>
     </section>
 
-    <footer class="ftco-footer bg-bottom" style="background-image: url(img/footer-bg.jpg);">
-      <div class="container">
-        <div class="row mb-5">
+    <!-- <footer class="ftco-footer bg-bottom" style="background-image: url(img/footer-bg.jpg -->
+    <footer class="ftco-footer bg-bottom bg-dark">
+        <div class="container">
+        <!-- <div class="row mb-5">
           <div class="col-md">
             <div class="ftco-footer-widget mb-4">
               <h2 class="ftco-heading-2">Vacation</h2>
@@ -382,56 +305,70 @@
             </div>
           </div>
           <div class="col-md">
-            <div class="ftco-footer-widget mb-4 ml-md-5">
-              <h2 class="ftco-heading-2">Infromation</h2>
-              <ul class="list-unstyled">
-                <li><a href="#" class="py-2 d-block">Online Enquiry</a></li>
-                <li><a href="#" class="py-2 d-block">General Enquiries</a></li>
-                <li><a href="#" class="py-2 d-block">Booking Conditions</a></li>
-                <li><a href="#" class="py-2 d-block">Privacy and Policy</a></li>
-                <li><a href="#" class="py-2 d-block">Refund Policy</a></li>
-                <li><a href="#" class="py-2 d-block">Call Us</a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-md">
              <div class="ftco-footer-widget mb-4">
               <h2 class="ftco-heading-2">Experience</h2>
               <ul class="list-unstyled">
                 <li><a href="#" class="py-2 d-block">Adventure</a></li>
                 <li><a href="#" class="py-2 d-block">Hotel and Restaurant</a></li>
                 <li><a href="#" class="py-2 d-block">Beach</a></li>
-                <li><a href="#" class="py-2 d-block">Nature</a></li>
-                <li><a href="#" class="py-2 d-block">Camping</a></li>
-                <li><a href="#" class="py-2 d-block">Party</a></li>
               </ul>
             </div>
           </div>
           <div class="col-md">
             <div class="ftco-footer-widget mb-4">
-            	<h2 class="ftco-heading-2">Have a Questions?</h2>
+            	<h2 class="ftco-heading-2">Questions?</h2>
             	<div class="block-23 mb-3">
 	              <ul>
-	                <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
-	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
+	                <li><span class="icon icon-map-marker"></span><span class="text">Frederick Terman Street, 3 29590 Málaga, Spain</span></li>
+	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+34 655 245 431</span></a></li>
+	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@cryptoaway.com</span></a></li>
 	              </ul>
 	            </div>
             </div>
           </div>
-        </div>
-        <div class="row">
-          <div class="col-md-12 text-center">
+        </div> -->
 
-            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-          </div>
+			<div class="row">
+
+				<div class="ml-auto col-lg-5 col-md-6 col-12">
+					<h2 class="mb-4 pb-2" data-aos="fade-up" data-aos-delay="200">Feel free to ask anything</h2>
+
+					<form action="#" method="post" class="contact-form webform" data-aos="fade-up" data-aos-delay="400" role="form">
+						<input type="text" class="form-control" name="cf-name" placeholder="Name">
+
+						<input type="email" class="form-control" name="cf-email" placeholder="Email">
+
+						<textarea class="form-control" rows="5" name="cf-message" placeholder="Message"></textarea>
+
+						<button type="submit" class="form-control" id="submit-button" name="submit">Send Message</button>
+					</form>
+				</div>
+
+				<div class="mx-auto mt-4 mt-lg-0 mt-md-0 col-lg-5 col-md-6 col-12">
+					<h2 class="mb-4" data-aos="fade-up" data-aos-delay="600">Where you can <span>find us</span></h2>
+
+					<p data-aos="fade-up" data-aos-delay="800"><i class="fa fa-map-marker mr-1"></i> Frederick Terman Street, 3 29590 Málaga, Spain</p>
+					<div class="google-map" data-aos="fade-up" data-aos-delay="900">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1696.0974942074547!2d-4.555483546224307!3d36.740719390117164!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd72f1d06d8246c9%3A0xb6dccfba3e65a1f2!2sFP%20IES%20Campanillas%20PTA!5e1!3m2!1ses!2ses!4v1652297987451!5m2!1ses!2ses" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>   
+					</div>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-md">
+            		<div class="ftco-footer-widget mb-4">
+            			<h2 class="ftco-heading-2">Questions?</h2>
+            			<div class="block-23 mb-3">
+	        		      <ul>
+	        		        <li><a href="#"><span class="icon icon-phone"></span><span class="text">+34 655 245 431</span></a></li>
+	        		        <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@cryptoaway.com</span></a></li>
+	        		      </ul>
+	        		    </div>
+            		</div>
+            	</div>
+			</div>
         </div>
-      </div>
     </footer>
-    
-  
 
   <!-- loader -->
   <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
