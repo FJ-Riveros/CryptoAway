51edb882bd4ec2848ad6a4d754a0897b30ebc839
FJ-Riveros <fran.etnies@gmail.com>
2022-06-01 23:57:13 +0200

    Minor changes to the cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,11 +1,11 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-// header('Access-Control-Allow-Origin: http://localhost:3000/');
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Origin: http://localhost:3000');
 
 
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Headers: *');
+// header('Access-Control-Allow-Methods: *');
+// header('Access-Control-Allow-Headers: *');
 /*
 |--------------------------------------------------------------------------
 | Create The Application
