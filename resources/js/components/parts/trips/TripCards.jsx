b776f637a9665ddcb81df1686c4679338386fc21
FJ-Riveros <fran.etnies@gmail.com>
2022-06-15 14:36:02 +0200

    Updating the styles

--- resources/js/components/parts/trips/TripCards.jsx
+++ resources/js/components/parts/trips/TripCards.jsx
@@ -11,39 +11,42 @@ function TripCards({TripBlockchainInfo, TripDBInfo, buyTrip, ownTrip}) {
     } 
 
     return (
-            <div data-toggle="collapse" data-target={`#collapse${TripDBInfo.idTrip}`} aria-expanded="false" aria-controls={`collapse${TripDBInfo.idTrip}`}>
+        <>
+            
                 <div className="tripCard mt-3">
                     <div className="row w-100">
-                        <div className="col-5">
-                            <div className="trip__image" style={{backgroundImage: `url(${TripDBInfo.img})`}}>
+                            <div className="col-5" style={{paddingLeft: "0"}}>
+                            <div data-toggle="collapse" data-target={`#collapse${TripDBInfo.idTrip}`} aria-expanded="false" aria-controls={`collapse${TripDBInfo.idTrip}`}>
+                                <div className="trip__image" style={{backgroundImage: `url(${TripDBInfo.img})`}}>
 
+                                </div>
                             </div>
                         </div>
                         <div className="col-7">
                             <h2>{TripBlockchainInfo.location}</h2>
-                            <h4>{TripBlockchainInfo.price}</h4>
-                            <span>{TripBlockchainInfo.groupSize} / {TripBlockchainInfo.actualSize}</span><br />
-                            <span>{TripDBInfo.startDate} - {TripDBInfo.endDate}</span>
+                            <span><i class="fa-solid fa-coins"></i><span className="ml-3">{TripBlockchainInfo.price}</span></span><br />
+                            <span><i class="fa-solid fa-users"></i><span className="ml-2"> {TripBlockchainInfo.groupSize} / {TripBlockchainInfo.actualSize}</span></span><br />
+                            <span><i class="fa-solid fa-plane-departure"></i><span style={{marginLeft: "0.8rem"}}>{TripDBInfo.startDate}</span></span><br />
+                            <span><i class="fa-solid fa-plane-arrival"></i><span style={{marginLeft: "0.8rem"}}>{TripDBInfo.endDate}</span></span><br />
+                            {ownTrip ?
+                            <span className="boughtTrip">Bought</span>
+                            :   TripBlockchainInfo.actualSize < TripBlockchainInfo.groupSize ?
+                                    <span className="buyTrip" onClick={()=> buyTrip(TripDBInfo.idTrip - 1, TripBlockchainInfo.price)}>Buy</span>
+                                :
+                                <span className="boughtTrip">Sold Out</span>
+                        }
                         </div>
                     </div>
-                    <div className="footer mt-2">
-                    </div>
-                    <div className="comments">
+                    <div className={`comments comment${TripDBInfo.idTrip}`}>
                         <div class="collapse" id={`collapse${TripDBInfo.idTrip}`}>
                             <p>{TripDBInfo.itinerary}</p>
                         </div>
-                        
-                        {ownTrip ?
-                            <span>Bought</span>
-                            :   TripBlockchainInfo.actualSize < TripBlockchainInfo.groupSize ?
-                                    <button onClick={()=> buyTrip(TripDBInfo.idTrip - 1, TripBlockchainInfo.price)}>Buy</button>
-                                :
-                                <span>Sold Out</span>
-                        }
+                                                
                     </div>
                 </div>
-            </div>
+        </>
     );
+    
 }
 
 export default TripCards;
